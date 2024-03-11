2edf567a0343770649b98cecb36a582fad692cd1
Dung-Pham <phamdunglili04012003@li04012003gmail.12003gmailcom>
2024-03-12 05:07:49 +0700

    add

---
+++ Component/GroupComponent.js
@@ -0,0 +1,32 @@
+import "./GroupComponent.css";
+
+const GroupComponent = () => {
+  return (
+    <div className="mask-group-parent">
+      <img
+        className="mask-group-icon4"
+        loading="lazy"
+        alt=""
+        src="/mask-group@2x.png"
+      />
+      <div className="frame-contacts-phone-email-add">
+        <img
+          className="frame-contacts-phone-email-add-child"
+          alt=""
+          src="/rectangle-21.svg"
+        />
+        <div className="frame-footer-info">
+          <div className="jun-20184">04 June 2019</div>
+        </div>
+        <b className="dude-youre-getting-container4">
+          <p className="blank-line7">&nbsp;</p>
+          <p className="online-platform-to1">
+            ONLINE PLATFORM TO BRIDGE IN LIVESTOCK.....
+          </p>
+        </b>
+      </div>
+    </div>
+  );
+};
+
+export default GroupComponent;
---
+++ Component/SwitchNode.js
@@ -0,0 +1,101 @@
+import Tag1InHomepage1 from "./Tag1InHomepage1";
+import Tag1InHomepage from "./Tag1InHomepage";
+import "./SwitchNode.css";
+
+const SwitchNode = () => {
+  return (
+    <div className="switch-node">
+      <div className="case-node">
+        <div className="tree-node">
+          <img className="base-icon1" alt="" src="/base-1@2x.png" />
+          <div className="b-s-t-node">
+            <div className="base2" />
+            <div className="a-v-l-tree-node">
+              <h1 className="hotels-how-to-container1">
+                <p className="sell-your-livestock">{`Sell your Livestock to `}</p>
+                <p className="potential-buyers-on">{`Potential Buyers on our `}</p>
+                <p className="platform-painlessly">Platform Painlessly</p>
+              </h1>
+            </div>
+            <div className="hash-table">
+              <div className="linked-list-node">
+                <div className="doubly-linked-list-node">
+                  <div className="hotels-how-to-container2">
+                    <p className="livestock-producer-merchant">
+                      Livestock producer, merchant or rancher’s sell his product
+                      and get good value
+                    </p>
+                    <p className="without-stress-and">
+                      without stress and any in exception of the middlemen bugs.
+                    </p>
+                  </div>
+                </div>
+                <b className="hotels-how-to-container3">
+                  <p className="are-you-a">
+                    Are you a livestock merchant, trader, or producer?
+                  </p>
+                  <p className="p2"></p>
+                  <p className="are-you-a1">Are you a rancher?</p>
+                  <p className="p3"></p>
+                  <p className="do-you-need">
+                    Do you need a platform to sell your livestock that works
+                    24/7 all day every day?
+                  </p>
+                  <p className="p4"></p>
+                </b>
+              </div>
+            </div>
+            <button className="mask-parent">
+              <div className="mask6" />
+              <div className="learn-more">Learn More</div>
+            </button>
+          </div>
+        </div>
+        <div className="max-heap-node">
+          <h1 className="hotels-how-to">Chuyên khoa</h1>
+          <div className="binary-max-heap-node">
+            <Tag1InHomepage1 />
+            <Tag1InHomepage1 />
+            <Tag1InHomepage1 />
+            <Tag1InHomepage1 />
+          </div>
+        </div>
+        <div className="break-point">
+          <div className="continue-node">
+            <h1 className="hotels-how-to1">Cẩm nang</h1>
+            <div className="call-function-node">
+              <Tag1InHomepage1 />
+              <Tag1InHomepage1 />
+              <Tag1InHomepage1 />
+              <Tag1InHomepage />
+            </div>
+          </div>
+        </div>
+        <div className="break-point1">
+          <div className="hotels-how-to-get-fr-parent">
+            <h1 className="hotels-how-to2">Bác sĩ</h1>
+            <div className="tag-1-in-homepage-parent">
+              <Tag1InHomepage1 />
+              <Tag1InHomepage1 />
+              <Tag1InHomepage1 />
+              <Tag1InHomepage />
+            </div>
+          </div>
+        </div>
+        <div className="break-point2">
+          <div className="hotels-how-to-get-fr-group">
+            <h1 className="hotels-how-to3">Cẩm nang</h1>
+            <div className="tag-1-in-homepage-group">
+              <Tag1InHomepage1 />
+              <Tag1InHomepage1 />
+              <Tag1InHomepage1 />
+              <Tag1InHomepage />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SwitchNode;
